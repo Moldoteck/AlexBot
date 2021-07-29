9a5fd9e86aed5693c744d79180af08bf6dd4605e
Moldoteck <cpadureac@gmail.com>
2021-07-29 23:12:20 +0300

    better command creation

--- src/handlers/sendHelp.ts
+++ src/handlers/sendHelp.ts
@@ -1,6 +1,5 @@
 import { Context } from 'telegraf'
 
 export function sendHelp(ctx: Context) {
-  ctx.setMyCommands([{ command: 'help', description: 'get help' }])
   return ctx.replyWithHTML(ctx.i18n.t('help'))
 }
--- src/middlewares/attachUser.ts
+++ src/middlewares/attachUser.ts
@@ -3,5 +3,6 @@ import { Context } from 'telegraf'
 
 export async function attachUser(ctx: Context, next: () => void) {
   ctx.dbuser = await findUser(ctx.from.id)
+  ctx.setMyCommands([{ command: 'help', description: 'get help' }])
   return next()
 }
